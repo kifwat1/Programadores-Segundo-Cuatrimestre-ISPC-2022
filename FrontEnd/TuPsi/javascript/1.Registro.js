09f809ee92e52d4f5eee50a1253ebf523a915252
pdiaz13 <prisciladiazva@gmail.com>
2022-11-02 20:55:08 -0300

    agrego js y edito registro

--- FrontEnd/TuPsi/javascript/1.Registro.js
+++ FrontEnd/TuPsi/javascript/1.Registro.js
@@ -6,4 +6,5 @@ form.addEventListener('submit', async function getFormData(e) {
         for (const pair of formData.entries()) {
             console.log(`${pair[0]}, ${pair[1]}`);
             }
-})
+}
+)
